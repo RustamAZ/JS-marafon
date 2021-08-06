a9c1aa769ba97603a6262f101c51783c5babaa60
RustamAZ <himynameisernie@yandex.ru>
2021-08-06 11:59:06 +0300

    Fix js logic

--- 3/app.js
+++ 3/app.js
@@ -1,13 +1,26 @@
 const $upbtn = document.querySelector('.up-button')
 const $downbtn = document.querySelector('.down-button')
 const $sidebar = document.querySelector('.sidebar')
-const $mainSlide = document.querySelector('.main-slide')
-const slidesCount = $mainSlide.querySelectorAll('div').length
+const $mainSlide = document.querySelector('.main')
+const $slides = $mainSlide.querySelectorAll('div')
+const slidesCount = $slides.length
 const $container = document.querySelector('.container')
-
+const $sidebarTitles = document.querySelectorAll('.sidebar__title')
 $sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
 
 let activeSlide = 0
+
+let isActive = false
+
+$sidebarTitles.forEach(title => {
+    title.addEventListener('click', () => {
+        $slides.forEach(slide => {
+            slide.classList.toggle('active')
+            isActive = true
+        })
+    })
+})
+
 
 $upbtn.addEventListener('click', () => {
     changeBlock('up')
@@ -32,6 +45,13 @@ function changeBlock(direction) {
     
     const height = $container.clientHeight
     
+    $slides.forEach(slide => {
+        if(isActive) {
+            slide.classList.remove('active')
+        }
+    })
+    
     $mainSlide.style.transform = `translateY(-${activeSlide * height}px)`
+    
     $sidebar.style.transform = `translateY(${activeSlide * height}px)`
 }
