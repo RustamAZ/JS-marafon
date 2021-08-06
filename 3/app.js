065690562be84a6f459b7a7b8a15e3714aac4ec4
RustamAZ <himynameisernie@yandex.ru>
2021-08-06 14:49:40 +0300

    Fix js logic

--- 3/app.js
+++ 3/app.js
@@ -4,6 +4,7 @@ const $sidebar = document.querySelector('.sidebar')
 const $mainSlide = document.querySelector('.main')
 const $slides = $mainSlide.querySelectorAll('div')
 const slidesCount = $slides.length
+console.log(slidesCount)
 const $container = document.querySelector('.container')
 const $sidebarTitles = document.querySelectorAll('.sidebar__title')
 $sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
@@ -43,15 +44,35 @@ function changeBlock(direction) {
         }
     }
     
+    delayAnimaionForSwap()
+}
+
+const delay = timeDelay => {
+    return new Promise(resolve => setTimeout(() => resolve(), timeDelay))
+}
+
+function delayAnimaionForSwap() {
+    closeBlock()
+    if (isActive) {
+        delay(500).then(responce => swapSlide())
+    } else {
+        swapSlide()
+    }
+    
+}
+
+function swapSlide() {
     const height = $container.clientHeight
     
+    $mainSlide.style.transform = `translateY(-${activeSlide * height}px)`
+    
+    $sidebar.style.transform = `translateY(${activeSlide * height}px)`
+}
+
+function closeBlock() {
     $slides.forEach(slide => {
         if(isActive) {
             slide.classList.remove('active')
         }
     })
-    
-    $mainSlide.style.transform = `translateY(-${activeSlide * height}px)`
-    
-    $sidebar.style.transform = `translateY(${activeSlide * height}px)`
 }
